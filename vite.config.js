063a78f0c87ccdc3745a1a76c09acce60057b967
suresh304 <sureshallie@gmail.com>
2025-07-09 23:07:07 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+
 
 // No need to import tailwindcss separately here – handled via PostCSS
 export default defineConfig({
