23ae377949e5d5a33b9634f950869962a22d5165
suresh304 <sureshallie@gmail.com>
2025-07-09 22:59:56 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// No need to import tailwindcss separately here – handled via PostCSS
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    allowedHosts: [
-      'beb1-115-98-236-35.ngrok-free.app' // 👈 Add your current ngrok hostname here
-    ]
-  }
 })
